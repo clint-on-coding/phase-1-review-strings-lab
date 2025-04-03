e0d642dbc88cac3951664fadf799daeae2962e66
Clinton Mwangi <clintonmwangi10636@gmail.com>
2025-04-03 09:42:34 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+// Define the current user
+const currentUser = 'Grace Hopper';
+
+// Create the welcome message using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create an excited welcome message in uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting using only the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
